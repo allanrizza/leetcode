26f0fd6cf47ce86e8c2adb7eb4adc36ef75c8b88
Allan Rizza <allanrizza.dev@gmail.com>
2023-03-26 22:22:06 -0300

    validparentheses

---
+++ validparentheses.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() <= 1) return false;
+
+        Stack stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            switch(s.charAt(i)) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(s.charAt(i));
+                    break;
+                case ')':
+                case ']':
+                case '}':
+                if(stack.size() == 0) return false;
+                    if(!stack.isEmpty()) {
+                        Character pop = (Character) stack.pop();
+                        if ((s.charAt(i) == ')' && pop != '(') ||
+                        (s.charAt(i) == ']' && pop != '[') ||
+                        (s.charAt(i) == '}' && pop != '{')) {
+                            return false;
+                        }
+                    }
+            }
+        }
+        if(stack.isEmpty()) return true;
+        else return false;
+    }
+}
