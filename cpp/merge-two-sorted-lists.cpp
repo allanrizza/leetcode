a5951dde8c8f16c3f38ea372624814b8e3b6a938
Allan Gilbert <allanrizza@gmail.com>
2025-01-01 16:01:22 -0300

    merge two sorted lists

---
+++ cpp/merge-two-sorted-lists.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main() {
+
+    
+
+    
+
+    return 0;
+}
+
+class Solution {
+public:
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *res, *temp;
+        res = new ListNode();
+        temp = res;
+
+        while(list1 && list2) {
+            if(list1->val >= list2->val) {
+                temp->next = list2;
+                list2 = list2->next;
+            } else {
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            temp = temp->next;
+        }
+        if(list1) {
+            temp->next = list1;
+        }
+        if(list2) {
+            temp->next = list2;
+        }
+
+        return res->next;
+    }
+};
